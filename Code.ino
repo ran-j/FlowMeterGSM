51ebc1bc110ffa818d220261a482985b949e893c
Ranieri <ran-junior@hotmail.com>
2018-06-22 18:05:43 -0300

    Realizado correção no código

--- Code.ino
+++ Code.ino
@@ -10,44 +10,11 @@ static struct pt pt1;
 FlowMeter Meter = FlowMeter(2);
 const unsigned long period = 1000;
 HTTP http(9600, RX_PIN, TX_PIN, RST_PIN);
+char *dtostrf(double val, signed char width, unsigned char prec, char *s);
 
 void MeterISR() {
   // let our flow meter count the pulses
   Meter.count();
-}
-
-void Post(){
-    http.configureBearer("movistar.es");
-    http.connect();
-    char response[256];
-
-    char myJson[80];
-    char jsonInicio[16]="{\"data\":";
-     
-    char meioJson[5] = "\"";
-    char finalJ[5] = "\"\}";
-    
-    sprintf(myJson,"%s %s %s %s",jsonInicio,meioJson,Meter.getCurrentFlowrate(),finalJ);
- 
-    Result result = http.post("www.rannet.com.br/postrannet", myJson, response);
-
-    Serial.println(response);
-
-    http.disconnect();
-}
-
-static int protothread1(struct pt *pt, int interval) {
-  static unsigned long timestamp = 0;
-  PT_BEGIN(pt);
-  while(1) { // never stop 
-    /* each time the function is called the second boolean
-    *  argument "millis() - timestamp > interval" is re-evaluated
-    *  and if false the function exits after that. */
-    PT_WAIT_UNTIL(pt, millis() - timestamp > interval );
-    timestamp = millis(); // take a new timestamp
-    Post();
-  }
-  PT_END(pt);
 }
 
 void setup() {
@@ -64,7 +31,21 @@ void loop() {
     Meter.tick(period);
 
     if(Meter.getCurrentFlowrate() > 10){
-        protothread1(&pt1, 200);     
+        Serial.println(Meter.getCurrentFlowrate());
+         http.configureBearer("movistar.es");
+          http.connect();
+          char response[256];
+       
+          char str_temp[6];
+          char buf [256];  
+          dtostrf(Meter.getCurrentFlowrate(), 4, 2, str_temp);
+          sprintf(buf,"{\"data\":\ %s }", str_temp);
+          
+          Result result = http.post("www.rannet.com.br/postrannet", buf, response);
+      
+          Serial.println(response);
+      
+          http.disconnect();
     }
      
 }
